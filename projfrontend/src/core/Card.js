cf38486b0ca220eabf41438c8437f701a29560b5
unknown <deepakkrathod3105@gmail.com>
2020-11-16 14:59:54 +0530

    handled frontend and fetched data from backend

---
+++ projfrontend/src/core/Card.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import ImageHelper from './helper/ImageHelper';
+
+    const Card = ({product,addtoCart = true,removeFromcart = false}) => {
+
+        const cardTitle = product ? product.name: "A photo from pixels"
+        const cardDescription = product ? product.description: "Default Description"
+        const cardPrice = product ? product.price: "DEFAULT"
+
+
+        const showAddToCart = (addtoCart) =>{
+            return(
+                addtoCart && (
+                    <button
+                    onClick={() => {}}
+                    className="btn btn-block btn-outline-success mt-2 mb-2"
+                  >
+                    Add to Cart
+                  </button>
+                )
+            )
+
+        }
+
+        const showremoveFromCart = (removeFromcart) =>{
+            return(
+                removeFromcart && (
+                    <button
+                    onClick={() => {}}
+                    className="btn btn-block btn-outline-danger mt-2 mb-2"
+                  >
+                    Remove from cart
+                  </button>
+                )
+            )
+
+        }
+
+        return (
+          <div className="card text-white bg-dark border border-info ">
+            <div className="card-header lead">{cardTitle}</div>
+            <div className="card-body">
+             <ImageHelper product={product}/>
+              <p className="lead bg-success font-weight-normal text-wrap">
+                {cardDescription}
+              </p>
+        <p className="btn btn-success rounded  btn-sm px-4">${cardPrice}</p>
+              <div className="row">
+                <div className="col-12">
+                  {showAddToCart(addtoCart)}
+                </div>
+                <div className="col-12">
+                  {showremoveFromCart(removeFromcart)}
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      }
+
+export default Card;
---
+++ projfrontend/src/core/Home.js
@@ -0,0 +1,43 @@
+import React,{useState,useEffect} from 'react'
+import {API} from "../backend"
+import Base from "./Base"
+import "../styles.css"
+import Card from './Card'
+import { getProducts } from './helper/coreapicalls'
+
+export default function Home() {
+
+    const [products,setProducts]= useState([])
+    const [error,setError]= useState(false)
+
+    const loadAllProducts = () =>{
+        getProducts().then(data =>{
+            if(data.error){
+                setError(data.error)
+            } else {
+                setProducts(data)
+            }
+        })
+    }
+
+    useEffect(()=>{
+        loadAllProducts()
+    },[])
+    
+    return (
+        <Base title="Home page" description="Welcome to the Tshirt Store">
+            <div className="row text-center">
+            <h1 className="text-white"> ALl of Tshirts</h1>
+            <div className="row">
+                {products.map((product,index)=>{
+                    return(
+                        <div key={index} className="col-4 mb-4">
+                            <Card product={product}/>
+                        </div>
+                    )
+                })}
+            </div>
+            </div>
+        </Base>
+    )
+}
