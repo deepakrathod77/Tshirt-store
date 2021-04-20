53ea311083489d0f2bb5c70ec20cda68f5d0ffd9
Deepak Rathod <deepakkrathod3105@gmail.com>
2021-04-20 11:43:09 +0530

    added frontend part

--- projfrontend/src/core/Card.js
+++ projfrontend/src/core/Card.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { Redirect } from 'react-router-dom';
+import { addItemToCart, removeItemToCart } from './helper/cartHelper';
 import ImageHelper from './helper/ImageHelper';
 
-    const Card = ({product,addtoCart = true,removeFromcart = false}) => {
+    const Card = ({product,addtoCart = true,removeFromCart = false, setReload = f => f, reload = undefined}) => {
+      
+      const [redirect, setRedirect] = useState(false)
+      const [count, setCount] = useState(product.count)
 
         const cardTitle = product ? product.name: "A photo from pixels"
         const cardDescription = product ? product.description: "Default Description"
         const cardPrice = product ? product.price: "DEFAULT"
 
+        const addToCart = () =>{
+          addItemToCart(product,()=>setRedirect(true))
+        }
+        const getARedirect = (redirect) =>{
+          if(redirect){
+            return <Redirect to="/cart"/>
+          }
+        }
 
         const showAddToCart = (addtoCart) =>{
             return(
                 addtoCart && (
                     <button
-                    onClick={() => {}}
+                    onClick= {addToCart}
                     className="btn btn-block btn-outline-success mt-2 mb-2"
                   >
                     Add to Cart
@@ -22,11 +35,14 @@ import ImageHelper from './helper/ImageHelper';
 
         }
 
-        const showremoveFromCart = (removeFromcart) =>{
+        const showremoveFromCart = (removeFromCart) =>{
             return(
-                removeFromcart && (
+                removeFromCart && (
                     <button
-                    onClick={() => {}}
+                    onClick={() => {
+                      removeItemToCart(product._id)
+                      setReload(!reload)
+                    }}
                     className="btn btn-block btn-outline-danger mt-2 mb-2"
                   >
                     Remove from cart
@@ -40,17 +56,18 @@ import ImageHelper from './helper/ImageHelper';
           <div className="card text-white bg-dark border border-info ">
             <div className="card-header lead">{cardTitle}</div>
             <div className="card-body">
+              {getARedirect(redirect)}
              <ImageHelper product={product}/>
               <p className="lead bg-success font-weight-normal text-wrap">
                 {cardDescription}
               </p>
-        <p className="btn btn-success rounded  btn-sm px-4">Rs.{cardPrice}</p>
+              <p className="btn btn-success rounded  btn-sm px-4">Rs.{cardPrice}</p>
               <div className="row">
                 <div className="col-12">
                   {showAddToCart(addtoCart)}
                 </div>
                 <div className="col-12">
-                  {showremoveFromCart(removeFromcart)}
+                  {showremoveFromCart(removeFromCart)}
                 </div>
               </div>
             </div>
